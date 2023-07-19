915779621bb18e1f94d77c870f717e366ef53298
Amiinoz <amiinoz@Mohameds-MacBook-Air.local>
2023-07-18 20:52:11 -0600

    🎉 initial setup

---
+++ src/main.js
@@ -0,0 +1,21 @@
+TODO: Setup
+create a player
+don't let gravity fool you
+move your ass
+platforms
+background
+win or loose
+images
+scroll
+pits
+levels
+improvements
+
+
+const canvas = document.querySelector("canvas");
+
+console.log(canvas);
+
+const con = canvas.getContext("2d");
+
+console.log(con);
