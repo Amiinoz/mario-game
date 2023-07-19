f1a34903c172f38624fb014da4a7d589a2d4e136
Amiinoz <amiinoz@Mohameds-MacBook-Air.local>
2023-07-18 22:56:48 -0600

    🍃 don't let gravity fool you

--- src/main.js
+++ src/main.js
@@ -9,6 +9,8 @@ console.log(cntx);
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+const gravity = 1.5;
+
 // Create a player
 class Player {
   constructor() {
@@ -16,14 +18,41 @@ class Player {
       x: 100,
       y: 100,
     };
-    this.width = 45;
-    this.height = 45;
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
+    this.width = 30;
+    this.height = 30;
   }
   draw() {
     cntx.fillStyle = "gray";
     cntx.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
+
+  update() {
+    this.draw();
+    this.position.y += this.velocity.y;
+
+    // if (this.position.y + this.height.y + this.velocity.y <= canvas.height)
+    //   this.velocity.y += gravity;
+    // else this.velocity.y = 0;
+
+    if (this.position.y + this.height + this.velocity.y <= canvas.height) {
+      this.velocity.y += gravity;
+    } else {
+      this.velocity.y = 0;
+    }
+  }
 }
 
 const player = new Player();
-player.draw();
+
+function animate() {
+  requestAnimationFrame(animate);
+  // console.log('test"');
+  cntx.clearRect(0, 0, canvas.width, canvas.height);
+  player.update();
+}
+
+animate();
