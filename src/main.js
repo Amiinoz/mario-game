0927259be0e613489d86635a8bbda6f0deedc178
Amiinoz <amiinoz@Mohameds-MacBook-Air.local>
2023-07-19 23:12:44 -0600

    player movement

--- src/main.js
+++ src/main.js
@@ -32,11 +32,8 @@ class Player {
 
   update() {
     this.draw();
+    this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
-
-    // if (this.position.y + this.height.y + this.velocity.y <= canvas.height)
-    //   this.velocity.y += gravity;
-    // else this.velocity.y = 0;
 
     if (this.position.y + this.height + this.velocity.y <= canvas.height) {
       this.velocity.y += gravity;
@@ -47,12 +44,80 @@ class Player {
 }
 
 const player = new Player();
+const keys = {
+  right: {
+    pressed: false,
+  },
+  left: {
+    pressed: false,
+  },
+};
 
 function animate() {
   requestAnimationFrame(animate);
   // console.log('test"');
   cntx.clearRect(0, 0, canvas.width, canvas.height);
   player.update();
+
+  if (keys.right.pressed) {
+    player.velocity.x = 5;
+  } else if (keys.left.pressed) {
+    player.velocity.x = -5;
+  } else {
+    player.velocity.x = 0;
+  }
 }
 
 animate();
+
+addEventListener("keydown", ({ keyCode }) => {
+  // console.log(event);
+  console.log(keyCode);
+  switch (keyCode) {
+    case 65:
+      console.log("left");
+      keys.left.pressed = true;
+
+      break;
+
+    case 83:
+      console.log("down");
+      break;
+
+    case 68:
+      console.log("right");
+      keys.right.pressed = true;
+      break;
+
+    case 87:
+      console.log("up");
+      player.velocity.y -= 7;
+      break;
+  }
+});
+
+addEventListener("keyup", ({ keyCode }) => {
+  // console.log(event);
+  console.log(keyCode);
+  switch (keyCode) {
+    case 65:
+      console.log("left");
+      keys.left.pressed = false;
+      break;
+
+    case 83:
+      console.log("down");
+      break;
+
+    case 68:
+      console.log("right");
+      keys.right.pressed = false;
+
+      break;
+
+    case 87:
+      console.log("up");
+      player.velocity.y -= 20;
+      break;
+  }
+});
